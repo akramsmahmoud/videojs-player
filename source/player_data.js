34b1c18aacb8f4e8b4536918d1f4c0581bdc1988
akram mahmoud <akrammahmoud.web@gmail.com>
2022-10-20 00:18:13 +0200

    s

--- source/player_data.js
+++ source/player_data.js
@@ -2,7 +2,7 @@ const player_data_source = [
     {
         season_id: 0,
         series_id:0,
-        series_url: 'http://amssamples.streaming.mediaservices.windows.net/634cd01c-6822-4630-8444-8dd6279f94c6/CaminandesLlamaDrama4K.ism/manifest(format=m3u8-aapl)',
+        series_url: 'https://amssamples.streaming.mediaservices.windows.net/634cd01c-6822-4630-8444-8dd6279f94c6/CaminandesLlamaDrama4K.ism/manifest(format=m3u8-aapl)',
     },
     {
         season_id: 0,
@@ -17,11 +17,11 @@ const player_data_source = [
     {
         season_id: 1,
         series_id: 0,
-        series_url: 'http://amssamples.streaming.mediaservices.windows.net/634cd01c-6822-4630-8444-8dd6279f94c6/CaminandesLlamaDrama4K.ism/manifest(format=m3u8-aapl)'
+        series_url: 'https://amssamples.streaming.mediaservices.windows.net/634cd01c-6822-4630-8444-8dd6279f94c6/CaminandesLlamaDrama4K.ism/manifest(format=m3u8-aapl)'
     },
     {
         season_id: 2,
         series_id: 0,
-        series_url: 'http://amssamples.streaming.mediaservices.windows.net/634cd01c-6822-4630-8444-8dd6279f94c6/CaminandesLlamaDrama4K.ism/manifest(format=m3u8-aapl)'
+        series_url: 'https://amssamples.streaming.mediaservices.windows.net/634cd01c-6822-4630-8444-8dd6279f94c6/CaminandesLlamaDrama4K.ism/manifest(format=m3u8-aapl)'
     },
 ];
