4e4a1538812188b1bb4c987bded995f945c95795
akram mahmoud <akrammahmoud.web@gmail.com>
2022-10-20 00:09:34 +0200

    first commit

--- VideoForLifeJs.js
+++ VideoForLifeJs.js
@@ -5,114 +5,123 @@ var btnMute = document.getElementById('btnMute');
 var topProgressBar = document.getElementById('top-progress-bar');
 var bottomProgressBar = document.getElementById('bottom-progress-bar');
 var PlayButton = document.getElementById("PlayButton");
-localStorage.setItem('endBtn', `<i id="PlayButtonIcon" class="fas fa-redo-alt" aria-hidden="true"></i> &nbsp; Watch Again`)
-localStorage.setItem('pauseBtn', `<i id="PlayButtonIcon" class="fas fa-play" aria-hidden="true"></i> keep Watching `)
-localStorage.setItem('playBtn', `<i id="PlayButtonIcon" class="fas fa-pause" aria-hidden="true"></i> &nbsp; You're Watching `)
-localStorage.setItem('initialBtn', `<i id="PlayButtonIcon" class="fas fa-play"></i> &nbsp;Play Video`)
-var canvas = document.getElementById('canvasId');
-var context = canvas.getContext('2d');
+
+if (!localStorage.getItem('pauseBtn')){
+    localStorage.setItem('endBtn', `<span>&nbsp</span><span><i id="PlayButtonIcon" class="fas fa-redo-alt" aria-hidden="true"></i> &nbsp; Watch Again</span>`)
+    localStorage.setItem('playBtn', `<span></span><span><i id="PlayButtonIcon" class="fas fa-pause" aria-hidden="true"></i> &nbsp; You're Watching</span>`)
+    localStorage.setItem('initialBtn', `<span>&nbsp</span><span><i id="PlayButtonIcon" class="fas fa-play"></i> &nbsp;Play Video</span>`)
+    localStorage.setItem('pauseBtn', `<span>&nbsp</span><span><i id="PlayButtonIcon" class="fas fa-play" aria-hidden="true"></i> keep Watching</span>`)
+}
+//var canvas = document.getElementById('canvasId');
+//var context = canvas.getContext('2d');
 
 window.addEventListener('load', function() {
-
-    if (topProgressBar.value === 0) {
+    const lastPercentage = localStorage.getItem('lastPercentage');
+    if (!lastPercentage || lastPercentage === 0) {
         PlayButton.innerHTML = localStorage.getItem("initialBtn")
-    } else if (topProgressBar.value > 0 && topProgressBar.value <= 99) {
+    } else if (lastPercentage > 0 && lastPercentage <= 99) {
+        updatePauseBtn()
         PlayButton.innerHTML = localStorage.getItem('pauseBtn')
     } else {
-        PlayButton.innerHTML = localStorage.getItem('endBtn')
-        
-        
+        PlayButton.innerHTML = localStorage.getItem('endBtn')        
     }
- // for canvas draw
-    if (localStorage.getItem("image")) {
+    // for canvas draw
+    /* if (localStorage.getItem("image")) {
         let img = new Image();
         img.src = localStorage.getItem("image");
 
         img.onload = function() {
             context.drawImage(img, 0, 0);
         };
-    }
- // for video watch count
-   if(localStorage.getItem('watchCount') > 0 ){
-       document.querySelector('#watched .status').innerHTML  ='Views'
-    document.querySelector('#watched .statusCount').innerHTML =  localStorage.getItem('watchCount') 
-      
-   }
- 
+    } */
 
 })
 
 
-player.addEventListener('timeupdate', updateProgressBar, false);
-player.addEventListener('play', function() {
-     player.onended = function(){
-      if(document.querySelector('#watched .statusCount').innerText == ''){
-            document.querySelector('#watched .statusCount').innerHTML =0
-            document.querySelector('#watched .status').innerHTML = "Views"
-        }
-      document.querySelector('#watched .statusCount').innerHTML =  parseInt(document.querySelector('#watched .statusCount').innerHTML) + 1
-    localStorage.setItem('watchCount',document.querySelector('#watched .statusCount').innerText)
-    }
-    PlayButton.innerHTML = localStorage.getItem("playBtn")
-     },false)
-    
-
-    
-
-
-player.addEventListener('pause', function() {
-   
-
-    PlayButton.innerHTML = localStorage.getItem('pauseBtn')
-}, false);
-
-player.addEventListener('ended', function() {
-    PlayButton.innerHTML = localStorage.getItem('endBtn')
-   
-
-
-    this.pause();
-}, false);
+function updatePauseBtn() {
+    const lastVideoWatched = JSON.parse(localStorage.getItem('lastVideoWatched'))
+    const MAX_LENGTH = 200;
+    const videoSeason = lastVideoWatched.currentSeason;
+    localStorage.setItem('pauseBtn', `<span>Season ${videoSeason+1}</span> <span><i id="PlayButtonIcon" class="fas fa-play" aria-hidden="true"></i> keep Watching</span>`)
+}
 
 
 
-function playPauseVideo() {
+function playPauseVideo($currentVideo, currentSeason, currentSeries) {
+    $currentVideo.addEventListener('timeupdate', () => updateProgressBar($currentVideo, currentSeason, currentSeries), false);
+    $currentVideo.addEventListener('play', function() {
+        PlayButton.innerHTML = localStorage.getItem("playBtn")
+    },false)
+
+    $currentVideo.addEventListener('pause', function() {
+        updatePauseBtn()
+        PlayButton.innerHTML = localStorage.getItem('pauseBtn')
+    }, false);
+
+    $currentVideo.addEventListener('ended', function() {
+        PlayButton.innerHTML = localStorage.getItem('endBtn')
+       // this.pause();
+    }, false);
+
+
     var element = document.getElementById("PlayButtonIcon");
-    if (player.paused || player.ended) {
+    if ($currentVideo.paused || $currentVideo.ended) {
         //debugger;
         var isClassExist = hasClass(element, "fa-play");
         if (isClassExist === true) {
             removeClass(element, "fa-play");
             addClass(element, "fa-pause");
-
+            updatePauseBtn()
             PlayButton.innerHTML = localStorage.getItem('pauseBtn')
 
         }
-        player.play();
+        $currentVideo.play();
     } else {
         var isClassExist = hasClass(element, "fa-pause");
         if (isClassExist === true) {
             removeClass(element, "fa-pause");
             addClass(element, "fa-play");
-
+            updatePauseBtn()
             PlayButton.innerHTML = localStorage.getItem('pauseBtn')
         }
-        player.pause();
+        //player.pause();
     }
 }
 
 
-function updateProgressBar() {
-    var percentage = Math.floor((100 / player.duration) * player.currentTime);
-    topProgressBar.value = percentage;
-    bottomProgressBar.value = percentage;
-    topProgressBar.innerHTML = percentage + '% played';
-    bottomProgressBar.innerHTML = percentage + '% played';
-    localStorage.setItem("VideoWatchPercentage", percentage);
-    localStorage.setItem("VideoEndedTime", player.currentTime);
-     canvas.style.display = 'block';
-    draw(player, canvas);
+function updateProgressBar($currentVideo, currentSeason, currentSeries) {
+    var percentage = Math.floor((100 / $currentVideo.duration) * $currentVideo.currentTime);
+    const videoCurrentTime = $currentVideo.currentTime
+    const currentTopProgressBar = document.querySelector('.modal-player__series--playing #top-progress-bar');
+    if(currentTopProgressBar){
+        currentTopProgressBar.value = percentage;
+        currentTopProgressBar.innerHTML = percentage + '% played';
+    }
+    
+    //Landing Progressbar
+    $active_video = document.querySelector('.playlist__series__playing');
+    $landingProgressBar = $active_video.querySelector('#top-progress-bar');
+    video_title = $active_video.nextElementSibling.innerText;
+    $landingProgressBar.value = percentage;
+    $landingProgressBar.innerHTML = percentage + '% played';
 
+    let last_registered_percentage = JSON.parse(localStorage.getItem("VideosWatchedPercentage"))
+    let season_watched_data = last_registered_percentage ? last_registered_percentage[currentSeason] : {};
+    season_watched_data = {
+        ...season_watched_data,
+        [currentSeries]: { percentage, videoCurrentTime}
+    }
+    if (last_registered_percentage && last_registered_percentage.hasOwnProperty(currentSeason)) {
+        last_registered_percentage[currentSeason] = season_watched_data;
+    }else{
+        last_registered_percentage = {
+            ...last_registered_percentage,
+            [currentSeason]: season_watched_data
+        }
+    }
+    localStorage.setItem("VideosWatchedPercentage", JSON.stringify(last_registered_percentage));
+    localStorage.setItem("lastVideoWatched", JSON.stringify({ currentSeason, currentSeries, video_title}));
+    localStorage.setItem("lastPercentage", percentage);
 }
 
 
@@ -207,23 +216,57 @@ function toggleFullScreen() {
     }
 }
 
-function draw(video, canvas) {
-
+/* function draw(video, canvas) {
     context.drawImage(video, 0, 0, canvas.width, canvas.height);
     localStorage.setItem("image", canvas.toDataURL("image/jpg"));
-
-
-}
+} */
 
 
 updateVideo1TimeWithBar();
 
 function updateVideo1TimeWithBar() {
-    var player = document.getElementById('gossVideo');
+    const watched_series_progress = JSON.parse(localStorage.getItem("VideosWatchedPercentage"))
+    const link = document.querySelector('[data-link="1"]');
+    /* var player = document.getElementById('gossVideo');
     topProgressBar.value = localStorage.getItem("VideoWatchPercentage");
     topProgressBar.innerHTML = localStorage.getItem("VideoWatchPercentage") + '% played';
-    bottomProgressBar.value = localStorage.getItem("VideoWatchPercentage");
-    bottomProgressBar.innerHTML = localStorage.getItem("VideoWatchPercentage") + '% played';
 
-    player.currentTime = parseFloat(localStorage.getItem("VideoEndedTime"));
+    player.currentTime = parseFloat(localStorage.getItem("VideoEndedTime")); */
+
+    videoThumbnails.forEach(($thumbnail) => {
+        const season = +$thumbnail.dataset.seasonIndex;
+        const series = +$thumbnail.dataset.seriesIndex;
+        if (watched_series_progress && watched_series_progress[season]){
+            if(watched_series_progress[season][series]){
+                currentPercentage = watched_series_progress[season][series].percentage;
+                $thumbnail.querySelector('#top-progress-bar').value = currentPercentage;
+                $thumbnail.querySelector('#top-progress-bar').innerHTML = currentPercentage + '% played';
+            }
+        }
+    });
+
+    videoItems.forEach(($videoItem) => {
+        $videoItem.querySelector('video').pause();
+        $videoItem.querySelector('video').currentTime = 0;
+        const season = +$videoItem.dataset.seasonIndex;
+        const series = +$videoItem.dataset.seriesIndex;
+
+        if (watched_series_progress && watched_series_progress[season]) {
+            if (watched_series_progress[season][series] && $videoItem.querySelector('video')) {
+                $videoItem.querySelector('video').currentTime = parseFloat(watched_series_progress[season][series].videoCurrentTime) || 0;
+            }
+        }
+    });
 }
+
+document.getElementById('PlayButton').addEventListener('click', () =>{
+    const lastWatchedVideo = JSON.parse(localStorage.getItem('lastVideoWatched'))
+    currentSeason = lastWatchedVideo?.currentSeason;
+    currentSeries = lastWatchedVideo?.currentSeries;
+
+    if (!currentSeason && !currentSeries){
+        playVideo(0,0)
+        return;
+    }
+    playVideo(currentSeason, currentSeries)
+})
---
+++ source/player_data.js
@@ -0,0 +1,27 @@
+const player_data_source = [
+    {
+        season_id: 0,
+        series_id:0,
+        series_url: 'http://devimages.apple.com/iphone/samples/bipbop/bipbopall.m3u8',
+    },
+    {
+        season_id: 0,
+        series_id: 1,
+        series_url: 'https://cph-p2p-msl.akamaized.net/hls/live/2000341/test/master.m3u8',
+    },
+    {
+        season_id: 0,
+        series_id: 2,
+        series_url: './media/S1-3.mp4'
+    },
+    {
+        season_id: 1,
+        series_id: 0,
+        series_url: 'http://amssamples.streaming.mediaservices.windows.net/634cd01c-6822-4630-8444-8dd6279f94c6/CaminandesLlamaDrama4K.ism/manifest(format=m3u8-aapl)'
+    },
+    {
+        season_id: 2,
+        series_id: 0,
+        series_url: 'http://amssamples.streaming.mediaservices.windows.net/634cd01c-6822-4630-8444-8dd6279f94c6/CaminandesLlamaDrama4K.ism/manifest(format=m3u8-aapl)'
+    },
+];
