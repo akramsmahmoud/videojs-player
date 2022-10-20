11e52b47c41480d2928a8c0ab8b19209e0b90b23
akram mahmoud <akrammahmoud.web@gmail.com>
2022-10-20 16:17:35 +0200

    fix

--- plugins/thumbnails-generator/index.js
+++ plugins/thumbnails-generator/index.js
@@ -265,6 +265,55 @@ const getVideoCover = (file, seekTo = 0.0) => {
 } 
 */
 
+const getVideoCover = (file, seekTo = 0.0) => {
+    return new Promise((resolve, reject) => {
+        // load the file to a video player
+        const videoPlayer = document.createElement('video');
+        videoPlayer.src = file;
+        videoPlayer.muted = true;
+        videoPlayer.load();
+        videoPlayer.addEventListener('error', (ex) => {
+            reject("error when loading video file", ex);
+        });
+        // load metadata of the video to get video duration and dimensions
+        videoPlayer.addEventListener('loadedmetadata', () => {
+            // seek to user defined timestamp (in seconds) if possible
+            if (videoPlayer.duration < seekTo) {
+                reject("video is too short.");
+                return;
+            }
+            // delay seeking or else 'seeked' event won't fire on Safari
+            setTimeout(() => {
+                videoPlayer.currentTime = seekTo;
+            }, 200);
+            // extract video thumbnail once seeking is complete
+            videoPlayer.addEventListener('seeked', () => {
+                // console.log('video is now paused at %ss.', seekTo);
+                // define a canvas to have the same dimension as the video
+                const canvas = document.createElement("canvas");
+                canvas.width = 500;
+                canvas.height = 280;
+                // draw the video frame to canvas
+                const ctx = canvas.getContext("2d");
+                ctx.drawImage(videoPlayer, 0, 0, canvas.width, canvas.height);
+                // return the canvas image as a blob
+                ctx.canvas.toBlob(
+                    blob => {
+                        var reader = new FileReader();
+                        reader.readAsDataURL(blob);
+                        reader.onloadend = function () {
+                            var base64data = reader.result;
+                            resolve({ [seekTo]: { src: base64data } });
+                        }
+                    },
+                    "image/jpeg",
+                    1
+                );
+            });
+        });
+    });
+} 
+
 const getHlsVideoCover = (file, seekTo = 0.0) => {
     return new Promise((resolve, reject) => {
         // load the file to a video player
